f44e5c5286e4cfa372a378dfa7bea1da6dc2af88
ridhzie <ridhzie@gmail.com>
2023-08-29 12:11:51 +0800

    code security modification

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-## BruteDefence
+# BruteDefence
+
 The script was written to monitor, log and respond to suspicious attempts on a MariaDB server, specifically targeting unauthorized connection attempts.
 
-### 1. Monitoring script
+## 1. Monitoring script
 
 The first script (`monitor_mariadb.sh`) was designed to continously monitor the MariaDB status for access denied warnings. The functionality are as follows:
 
@@ -11,11 +12,11 @@ The first script (`monitor_mariadb.sh`) was designed to continously monitor the 
 - Blocking Threshold: Calculate the average attempts of the username and IP, and dynamically set the threshold to block the suspicious attempts.
 - Blocking IP: If the username and IP address surpassed the threshold, it is blocked using `iptables`.
 
+### POC
 
-#### POC
 ![POC](log_POC.png)
 
-### 2. Log rotation configuration
+## 2. Log rotation configuration
 
 The second part of the solution involves setting up log rotation for the log file created by the monitoring script. This is done using the `logrotate`. The functionality are as follows:
 
@@ -24,28 +25,28 @@ The second part of the solution involves setting up log rotation for the log fil
 - Error handling: Includes options to handle missing log files or empty log files gracefully.
 - Permission management: Ensures that new log files have the correct permissions and ownership.
 
-
-### HOW TO USE
+## HOW TO USE
 
 1. Make it executable: Run `chmod +x monitor_mariadb.sh`
 2. Run the script: `sudo ./monitor_mariadb.sh`
 
-### SETTING THE PROPER PERMISSIONS
+## SETTING THE PROPER PERMISSIONS
 
 ``sudo chmod 0644 /etc/logrotate.d/mariadb_access_attempts``
 
 ``sudo chown root:root /var/log/mariadb_access_attempts.log``
 
-### TESTING
+## TESTING
 
 ``sudo logrotate -d /etc/logrotate.d/mariadb_access_attempts``
 
-### MANUAL EXECUTION
+## MANUAL EXECUTION
 
 ``sudo logrotate /etc/logrotate.d/mariadb_access_attempts``
 
-### NOTES
+## NOTES
+
 1. The script use `journalctl` to monitor the MariaDB status, so it requires root privileges.
 2. The script logs the username and IPs and attempts to the specified log file and block the IP using `iptables` if the attempts are greater than the calculated threshold.
 
-Created and maintained by @ridhzie - https://github.com/ridhzie
+Created and maintained by @ridhzie - <https://github.com/ridhzie>
--- monitor_mariadb.sh
+++ monitor_mariadb.sh
@@ -5,17 +5,17 @@ LOG_FILE="/var/log/mariadb_access_attempts.log"
 
 # Function to log and block IPs
 block_ip() {
-  IP=$1
-  USERNAME=$2
+  IP="$1"
+  USERNAME="$2"
 
   # Get the current timestamp
   CURRENT_TIMESTAMP=$(date +%s)
 
   # Count attempts within the last 15 minutes
-  RECENT_ATTEMPTS=$(grep "$IP" $LOG_FILE | awk -v current="$CURRENT_TIMESTAMP" '{ split($4, a, "-"); if (a[1] > (current - 900)) print $0 }' | wc -l)
+  RECENT_ATTEMPTS=$(grep "$IP" "$LOG_FILE" | awk -v current="$CURRENT_TIMESTAMP" '{ split($4, a, "-"); if (a[1] > (current - 900)) print $0 }' | wc -l)
 
   # Calculate average attempts over the last hour
-  AVG_ATTEMPTS_LAST_HOUR=$(awk -v current="$CURRENT_TIMESTAMP" '{ split($4, a, "-"); if (a[1] > (current - 3600)) print $0 }' $LOG_FILE | wc -l)
+  AVG_ATTEMPTS_LAST_HOUR=$(awk -v current="$CURRENT_TIMESTAMP" '{ split($4, a, "-"); if (a[1] > (current - 3600)) print $0 }' "$LOG_FILE" | wc -l)
   AVG_ATTEMPTS_LAST_HOUR=$((AVG_ATTEMPTS_LAST_HOUR / 4))
 
   # Set the blocking threshold to half the average
@@ -23,25 +23,25 @@ block_ip() {
   BLOCKING_THRESHOLD=$((BLOCKING_THRESHOLD < 5 ? 5 : BLOCKING_THRESHOLD))
 
   # Log the IP, username, and timestamp
-  echo "$IP - $USERNAME - $(date +%s)" >> $LOG_FILE
+  echo "$IP - $USERNAME - $(date +%s)" >> "$LOG_FILE"
 
   # Block the IP if recent attempts exceed the dynamic threshold
-  if [ $RECENT_ATTEMPTS -gt $BLOCKING_THRESHOLD ]; then
-    sudo iptables -A INPUT -s $IP -j DROP
-    echo "Blocked IP: $IP targeting user: $USERNAME after exceeding threshold of $BLOCKING_THRESHOLD attempts within 15 minutes" >> $LOG_FILE
+  if [ "$RECENT_ATTEMPTS" -gt "$BLOCKING_THRESHOLD" ]; then
+    sudo iptables -A INPUT -s "$IP" -j DROP
+    echo "Blocked IP: $IP targeting user: $USERNAME after exceeding threshold of $BLOCKING_THRESHOLD attempts within 15 minutes" >> "$LOG_FILE"
   fi
 }
 
 # Monitor MariaDB status
 sudo journalctl -u mariadb -f |
   while IFS= read -r line; do
-    if [[ $line == *"[Warning] Access denied for user"* ]]; then
+    if [[ "$line" == *"[Warning] Access denied for user"* ]]; then
       # Extract the IP
-      IP=$(echo $line | rev | cut -d"'" -f2 | rev)
+      IP=$(echo "$line" | rev | cut -d"'" -f2 | rev)
       # Extract the username
-      USERNAME=$(echo $line | grep -oP "(?<=user ')[^']+" )
+      USERNAME=$(echo "$line" | grep -oP "(?<=user ')[^']+" )
 
       # Log, analyze, and block if necessary
-      block_ip $IP $USERNAME
+      block_ip "$IP" "$USERNAME"
     fi
   done
